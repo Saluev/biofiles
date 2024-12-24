9ab6073a701b6c7fbdb6ee80a0ae4b594e10d47f
Tigran Saluev <tigran@saluev.com>
2024-12-24 22:13:04 +0400

    Add GFF3 writer

--- tests/gff.py
+++ tests/gff.py
@@ -1,7 +1,8 @@
 import pathlib
+from io import StringIO
 
-from biofiles.gff import GFFReader
-from biofiles.types.feature import Gene, Exon
+from biofiles.gff import GFFReader, GFF3Writer
+from biofiles.types.feature import Gene, Exon, Feature
 
 
 def test_parse_liftoff_annotation() -> None:
@@ -18,3 +19,29 @@ def test_parse_gnomon_annotation() -> None:
         features = [*r]
     assert sum(1 for f in features if isinstance(f, Gene)) == 2
     assert sum(1 for f in features if isinstance(f, Exon)) == 9
+
+
+def test_write_gff3() -> None:
+    io = StringIO()
+    w = GFF3Writer(io)
+    w.write(
+        Feature(
+            sequence_id="chr1",
+            source="Imagination",
+            type_="gene",
+            start_original=70,
+            end_original=420,
+            start_c=69,
+            end_c=420,
+            score=None,
+            strand="+",
+            phase=None,
+            attributes={"biotype": "protein_coding", "foo": "bar"},
+            parent=None,
+            children=(),
+        )
+    )
+    assert (
+        io.getvalue()
+        == "##gff-version 3\nchr1\tImagination\tgene\t70\t420\t\t+\t\tbiotype=protein_coding;foo=bar\n"
+    )
